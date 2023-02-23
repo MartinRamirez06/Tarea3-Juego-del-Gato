911a50885cb9db183e4583ee32dd4a1f7f703ca6
MartinRamirez06 <125822553+MartinRamirez06@users.noreply.github.com>
2023-02-22 22:56:33 -0600

    Add files via upload

---
+++ Tablero_Gato/src/Main.java
@@ -0,0 +1,14 @@
+ public class Main {
+    public static void main(String[] args) {
+        String Col1 = ("___");
+        String Fils = ("||");
+
+        System.out.println("      "+Fils+"      "+Fils);
+        System.out.println(Col1+Col1+Fils+Col1+Col1+Fils+Col1+Col1);
+        System.out.println("      "+Fils+"      "+Fils);
+        System.out.println(Col1+Col1+Fils+Col1+Col1+Fils+Col1+Col1);
+        System.out.println("      "+Fils+"      "+Fils);
+        System.out.println("      "+Fils+"      "+Fils);
+    }
+}
+
